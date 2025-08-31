58d8953612d17db513d48c402df476c24823784b
Mohamad Karbejha <mohamad@techtile.com.tr>
2025-09-01 01:42:19 +0300

    fix: Update login link in Header component to point to the correct URL

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -50,7 +50,7 @@ export default function Header() {
                 <div className="hidden sm:ml-6 sm:flex sm:items-center sm:space-x-4">
                   <ThemeToggle />
                   <a
-                    href="https://flow-hr-seven.vercel.app/"
+                    href="https://login.karbejha.site"
                     className="text-gray-500 hover:text-gray-700 dark:text-gray-300 dark:hover:text-gray-200 text-sm font-medium"
                   >
                     Login
